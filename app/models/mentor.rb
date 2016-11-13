e3fd3aa23a6abc3669c190b955b440248d5aab37
DeniseCP <decpereira@hotmail.com>
2016-11-12 19:13:53 -0800

    my changes
    Mychanges

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,5 +1,5 @@
 class Mentor < ApplicationRecord
     mount_uploader :image, ImageUploader
-    geocoded_by :full_street_address   # can also be an IP address
+    geocoded_by :location   # can also be an IP address
     after_validation :geocode  
 end
